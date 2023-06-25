cf108a29df01fe5c06ffa574f56c2bf9165e4d77
JohanFernandoLema <johan.13fernando@gmail.com>
2023-06-24 22:09:40 -0400

    express server created and working

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 //MVC model view controller
-const http = require('http')
-const url = require('url')
+
+const express = require('express')
+const app = express()
 
 const port = 3000
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Hello World')
-  } else if (req.url === '/log-in') {
-    res.end(`<button>Log in</button>`)
-  }
+// Routing
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-server.listen(port)
+// Making the server go live
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+})
